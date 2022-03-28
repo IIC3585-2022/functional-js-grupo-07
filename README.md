8d0763854cbd26d5b8f36d592271a2aed1ecd60b
Matías Cadile <mjcadile@uc.cl>
2022-03-28 11:31:54 -0300

    fixes: refactor + piping/lodash on main flujo

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 [![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-f059dc9a6f8d3a56e377f745f24479a46679e63a5d9fe6f495e02850cd0d8118.svg)](https://classroom.github.com/online_ide?assignment_repo_id=7322986&assignment_repo_type=AssignmentRepo)
-# Tarea {} - Grupo{}
-Description
+# Tarea 1 - Grupo 7
+## Considerar los siguientes supuestos al momento de revisar el codigo:
+- El juego no termina hasta que alguno de los jugadores llegue exactamente a 0 puntos.
+- El input debe seguir un formato específico: _nombre1_,_nombre2_,_nombre3_,...
+- Se asume que el formato del input es correcto. 
--- T1/main.js
+++ T1/main.js
@@ -1,51 +1,35 @@
+const _ = require("lodash");
 const prompt = require("prompt-sync")({ sigint: true });
 
-function crearIterador(arreglo){
-    var siguienteIndice = 0;
 
-    return {
-       next: function(){
-           return siguienteIndice < arreglo.length ?
-               {value: arreglo[siguienteIndice++], done: false} :
-               {done: true};
-       }
-    }
-}
-
-
-function ValueShot(shot){
-
+function shot_value(shot){
     if (shot == "DB"){
         return 50;
     }else if(shot == "SB"){
         return 25;
-    }else{
-        return (shot[0]*shot[1]);
-    }   
-}
-
-function UpdatePoints(value, turn){
-    var iterador = crearIterador(turn);
-    value -= ValueShot(iterador.next().value);
-    value -= ValueShot(iterador.next().value);
-    value -= ValueShot(iterador.next().value);
-    if (value < 0 ){
-        value *= -1
     }
-    return value;
+    return (shot[0]*shot[1]); 
 }
 
-function initPlayers() {
+function update_points(currentPoints, turn){
+    currentPoints -= _.chain(turn).map(shot_value).sum();
+    return Math.abs(currentPoints);
+}
+
+const get_players = () => {
+    console.log("Ingresa los jugadores separados por una ´,´ (EJ: Matias,Jose,Pablo)");
+    var playerNames = prompt();
+    return playerNames.trim().split(",")
+}
+
+const init_players = (playerNames) => {
     var players = new Object();
-    var names = Array.from(arguments)
-  
-    names.map((x) => {players[x] = 501;});
-  
+    playerNames.map((x) => {players[x] = 501;});
     console.log("Juego inicializado con jugadores "+ Object.keys(players) +".");
     return players;
-  }
+}
 
-  function * turno(players) {
+function * turn_generator(players) {
     //console.log(players)
     names = Array.from(Object.keys(players));
     var i = 0;
@@ -59,18 +43,15 @@ function initPlayers() {
       }
     }
   }
-  
 
-async function main(){
-  
-    var players = await initPlayers("Jaime", "Clara");
+const init_game = (players) => {
     var winner = true;
-    const turnoGen = turno(players);
+    const turnGen = turn_generator(players);
     while (winner){
-        var currentPlayer = turnoGen.next().value;
+        var currentPlayer = turnGen.next().value;
         var turn = prompt("Ingrese lanzamientos de " + currentPlayer + ": ");
         turn = JSON.parse(turn);
-        players[currentPlayer] = await UpdatePoints(players[currentPlayer], turn);
+        players[currentPlayer] = update_points(players[currentPlayer], turn);
         console.log(currentPlayer + " queda con "+ players[currentPlayer] +" puntos");
         if (players[currentPlayer] == 0){
             console.log("y gana el juego. Felicitaciones "+ currentPlayer + " !!");
@@ -79,4 +60,8 @@ async function main(){
     }
 }
 
-main("Jaime", "Clara");
+// PIPE + LODASH
+const _pipe = (op1, op2) => (arg) => op2(op1(arg));
+const pipe = (...ops) => _.reduce(ops, _pipe);
+const game = pipe(get_players, init_players, init_game);
+game();
